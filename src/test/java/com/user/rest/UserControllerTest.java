6093685a6cdddbbee48a37ee6558786533ef6efc
HarishNallaballe <harishnallaballe@gmail.com>
2024-10-09 22:27:52 +0530

    test cases commented

--- src/test/java/com/user/rest/UserControllerTest.java
+++ src/test/java/com/user/rest/UserControllerTest.java
@@ -1,116 +1,116 @@
-package com.user.rest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.user.dto.UpdatePwd;
-import com.user.entity.User;
-import com.user.exception.UserNotFoudException;
-import com.user.rest.UserController;
-import com.user.service.UserService;
-
-@WebMvcTest(controllers = UserController.class)
-public class UserControllerTest {
-
-	@MockBean
-	private UserService userService;
-
-	@Autowired
-	private MockMvc mockMvc;
-
-//	ObjectMapper obj=new ObjectMapper();
-//	ObjectWriter objectWriter=obj.writer();
-	
-//	  @Test
-//	  public void checkEmailUnique() throws Exception {
-//	    
-//	  when(userService.checkEmailUnique(Mockito.anyString())).thenReturn("Duplicate Email");
-//		  
-//	  MockHttpServletRequestBuilder req =
-//	  MockMvcRequestBuilders.get("/unique/{email}","email");
-//	  
-//	  MvcResult result = mockMvc.perform(req).andReturn();
-//	  
-//	  MockHttpServletResponse response = result.getResponse();
-//	  
-//	  String contentAsString = response.getContentAsString();
-//	  System.out.println(contentAsString);
-//	  
-//	  //int status = response.getStatus();
-//	  
-//	  assertEquals("Duplicate Email", contentAsString);
-//	  
-//	  }
-//	  
-//	  @Test
-//	  public void forgotPwdTest() throws Exception {
-//		  when(userService.forgotPwd(Mockito.anyString())).thenReturn("harish123");
-//		  
-//		  MockHttpServletRequestBuilder req = MockMvcRequestBuilders.get("/forgot/{email}","email");
-//		  
-//		  MvcResult result = mockMvc.perform(req).andReturn();
-//		  
-//		  MockHttpServletResponse response = result.getResponse();
-//		  
-//		  String contentAsString = response.getContentAsString();
-//		  assertEquals("harish123", contentAsString);
-//	  }
-//	  
-//	  @Test
-//	  public void updatePwdTest() throws UserNotFoudException,Exception {
-//		  
-//		  UpdatePwd updatePwd = new UpdatePwd("harish123","nalla123","nalla123");
-//		  User user = new User();
-//		  BeanUtils.copyProperties(updatePwd, user);
-//		  String string = objectWriter.writeValueAsString(user);
-//		  when(userService.updatePwd(any(UpdatePwd.class))).thenReturn("Pwd");
-//		  
-//		  MockHttpServletRequestBuilder put = MockMvcRequestBuilders.put("/update")
-//				                                      .contentType(MediaType.APPLICATION_JSON)
-//				                                      .accept(MediaType.APPLICATION_JSON)
-//				                                      .content(string);
-//		  String contentAsString = mockMvc.perform(put).andReturn().getResponse()
-//				                                       .getContentAsString();
-//		  System.out.println(contentAsString);
-//		  assertEquals("Pwd", contentAsString);
-//				                          
-//	  }
+//package com.user.rest;
 //
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.Mockito.when;
+//
+//import org.junit.jupiter.api.Test;
+//import org.mockito.Mockito;
+//import org.springframework.beans.BeanUtils;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.MediaType;
+//import org.springframework.mock.web.MockHttpServletResponse;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MvcResult;
+//import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.ObjectWriter;
+//import com.user.dto.UpdatePwd;
+//import com.user.entity.User;
+//import com.user.exception.UserNotFoudException;
+//import com.user.rest.UserController;
+//import com.user.service.UserService;
+//
+//@WebMvcTest(controllers = UserController.class)
+//public class UserControllerTest {
+//
+//	@MockBean
+//	private UserService userService;
+//
+//	@Autowired
+//	private MockMvc mockMvc;
+//
+////	ObjectMapper obj=new ObjectMapper();
+////	ObjectWriter objectWriter=obj.writer();
 //	
-//		/*
-//		 * public void register() throws Exception {
-//		 * 
-//		 * UserDTO userdto = new UserDTO("harish","harish12@gmail.com","6300369259");
-//		 * 
-//		 * User user = new User();
-//		 * 
-//		 * BeanUtils.copyProperties(userdto, user);
-//		 * 
-//		 * String string = objectWriter.writeValueAsString(user);
-//		 * 
-//		 * MockHttpServletRequestBuilder req =
-//		 * MockMvcRequestBuilders.post("/register").contentType(MediaType.
-//		 * APPLICATION_JSON) .accept(MediaType.APPLICATION_JSON) .content(string);
-//		 * ResultActions result = mockMvc.perform(req).andExpect(status().isCreated())
-//		 * .andExpect(jsonPath("$", notNullValue()))
-//		 * .andExpect(jsonPath("$.name",is("harish"))); assertEquals(201, result); }
-//		 */
-//	
-//	
-}
+////	  @Test
+////	  public void checkEmailUnique() throws Exception {
+////	    
+////	  when(userService.checkEmailUnique(Mockito.anyString())).thenReturn("Duplicate Email");
+////		  
+////	  MockHttpServletRequestBuilder req =
+////	  MockMvcRequestBuilders.get("/unique/{email}","email");
+////	  
+////	  MvcResult result = mockMvc.perform(req).andReturn();
+////	  
+////	  MockHttpServletResponse response = result.getResponse();
+////	  
+////	  String contentAsString = response.getContentAsString();
+////	  System.out.println(contentAsString);
+////	  
+////	  //int status = response.getStatus();
+////	  
+////	  assertEquals("Duplicate Email", contentAsString);
+////	  
+////	  }
+////	  
+////	  @Test
+////	  public void forgotPwdTest() throws Exception {
+////		  when(userService.forgotPwd(Mockito.anyString())).thenReturn("harish123");
+////		  
+////		  MockHttpServletRequestBuilder req = MockMvcRequestBuilders.get("/forgot/{email}","email");
+////		  
+////		  MvcResult result = mockMvc.perform(req).andReturn();
+////		  
+////		  MockHttpServletResponse response = result.getResponse();
+////		  
+////		  String contentAsString = response.getContentAsString();
+////		  assertEquals("harish123", contentAsString);
+////	  }
+////	  
+////	  @Test
+////	  public void updatePwdTest() throws UserNotFoudException,Exception {
+////		  
+////		  UpdatePwd updatePwd = new UpdatePwd("harish123","nalla123","nalla123");
+////		  User user = new User();
+////		  BeanUtils.copyProperties(updatePwd, user);
+////		  String string = objectWriter.writeValueAsString(user);
+////		  when(userService.updatePwd(any(UpdatePwd.class))).thenReturn("Pwd");
+////		  
+////		  MockHttpServletRequestBuilder put = MockMvcRequestBuilders.put("/update")
+////				                                      .contentType(MediaType.APPLICATION_JSON)
+////				                                      .accept(MediaType.APPLICATION_JSON)
+////				                                      .content(string);
+////		  String contentAsString = mockMvc.perform(put).andReturn().getResponse()
+////				                                       .getContentAsString();
+////		  System.out.println(contentAsString);
+////		  assertEquals("Pwd", contentAsString);
+////				                          
+////	  }
+////
+////	
+////		/*
+////		 * public void register() throws Exception {
+////		 * 
+////		 * UserDTO userdto = new UserDTO("harish","harish12@gmail.com","6300369259");
+////		 * 
+////		 * User user = new User();
+////		 * 
+////		 * BeanUtils.copyProperties(userdto, user);
+////		 * 
+////		 * String string = objectWriter.writeValueAsString(user);
+////		 * 
+////		 * MockHttpServletRequestBuilder req =
+////		 * MockMvcRequestBuilders.post("/register").contentType(MediaType.
+////		 * APPLICATION_JSON) .accept(MediaType.APPLICATION_JSON) .content(string);
+////		 * ResultActions result = mockMvc.perform(req).andExpect(status().isCreated())
+////		 * .andExpect(jsonPath("$", notNullValue()))
+////		 * .andExpect(jsonPath("$.name",is("harish"))); assertEquals(201, result); }
+////		 */
+////	
+////	
+//}
--- src/test/java/com/user/service/UserServiceTest.java
+++ src/test/java/com/user/service/UserServiceTest.java
@@ -1,159 +1,159 @@
-package com.user.service;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import org.assertj.core.util.Arrays;
-import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.BeanUtils;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import com.user.dto.SignInDto;
-import com.user.dto.UpdatePwd;
-import com.user.dto.UserDTO;
-import com.user.entity.User;
-import com.user.exception.UserNotFoudException;
-import com.user.repo.UserRepository;
-
-@ExtendWith(MockitoExtension.class)
-@SpringBootTest
-public class UserServiceTest {
-	
-	@MockBean
-	private UserRepository userRepository;
-	
-	@InjectMocks
-	private UserServiceImpl userServiceImpl;
-	
-	@Test
-	public void registerUserTest_DuplicateEmail() {
-		UserDTO dto = new UserDTO(1,"hari","hari@gmail.com","6300369259");
-		User user = new User();
-		BeanUtils.copyProperties(dto, user);
-		when(userRepository.save(user)).thenReturn(user);
-		String message = userServiceImpl.register(dto);
-		assertThat("Already user present with given email id : hari@gmail.com").isEqualTo(message);
-	}
-	
+//package com.user.service;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.junit.Assert.assertThrows;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.ArgumentMatchers.anyString;
+//import static org.mockito.Mockito.when;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Optional;
+//
+//import org.assertj.core.util.Arrays;
+//import org.junit.Assert;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import org.springframework.beans.BeanUtils;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//
+//import com.user.dto.SignInDto;
+//import com.user.dto.UpdatePwd;
+//import com.user.dto.UserDTO;
+//import com.user.entity.User;
+//import com.user.exception.UserNotFoudException;
+//import com.user.repo.UserRepository;
+//
+//@ExtendWith(MockitoExtension.class)
+//@SpringBootTest
+//public class UserServiceTest {
+//	
+//	@MockBean
+//	private UserRepository userRepository;
+//	
+//	@InjectMocks
+//	private UserServiceImpl userServiceImpl;
+//	
 //	@Test
-//	public void registerUser_Savetest() {
-//		UserDTO dto = new UserDTO(1,"hari","hari1@gmail.com","6300369259");
+//	public void registerUserTest_DuplicateEmail() {
+//		UserDTO dto = new UserDTO(1,"hari","hari@gmail.com","6300369259");
 //		User user = new User();
 //		BeanUtils.copyProperties(dto, user);
 //		when(userRepository.save(user)).thenReturn(user);
 //		String message = userServiceImpl.register(dto);
-//		assertEquals("User Saved Successfully", message);
+//		assertThat("Already user present with given email id : hari@gmail.com").isEqualTo(message);
 //	}
-	
-	@Test
-	public void checkEmailUniqueTest() {
-		String email="kala1@gmail.com";
-		User user = new User();
-		when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
-		String checkEmailUnique = userServiceImpl.checkEmailUnique(email);
-		assertThat(checkEmailUnique).isEqualTo("Already user present with given email id : "+email);
-	}
-	
-	@Test
-	public void signInTest() throws UserNotFoudException {
-			SignInDto signInDto = new SignInDto();
-			signInDto.setEmail("shilpa@gmail.com");
-			signInDto.setPassword("Shil6610");
-			User user = new User();
-			when(userRepository.findByEmailAndConfirmPwd(signInDto.getEmail(), signInDto.getPassword())).thenReturn(Optional.of(user));
-			boolean signin = userServiceImpl.signin(signInDto);
-			assertEquals(true, signin);
-	}
-	
+//	
+////	@Test
+////	public void registerUser_Savetest() {
+////		UserDTO dto = new UserDTO(1,"hari","hari1@gmail.com","6300369259");
+////		User user = new User();
+////		BeanUtils.copyProperties(dto, user);
+////		when(userRepository.save(user)).thenReturn(user);
+////		String message = userServiceImpl.register(dto);
+////		assertEquals("User Saved Successfully", message);
+////	}
+//	
 //	@Test
-//	public void sigiTest() {
-//		SignInDto signInDto = new SignInDto();
-//		assertThrows(UserNotFoudException.class, ()->userServiceImpl.signin(signInDto));
-//	}
-
-	
-//	@Test
-//	public void forgotPasswordTest() throws UserNotFoudException {
+//	public void checkEmailUniqueTest() {
+//		String email="kala1@gmail.com";
 //		User user = new User();
-//		when(userRepository.findByEmail("kala1@gmail.com")).thenReturn(Optional.of(user));
-//		String forgotPwd = userServiceImpl.forgotPwd("kala1@gmail.com");
-//		System.out.println(forgotPwd);
-//		assertThat("Harish").isEqualTo(forgotPwd);
+//		when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
+//		String checkEmailUnique = userServiceImpl.checkEmailUnique(email);
+//		assertThat(checkEmailUnique).isEqualTo("Already user present with given email id : "+email);
 //	}
-	
-	@Test
-	public void getUsersTest() {
-		List<User> list=new ArrayList<>();
-				list.add(new User(1,"harish","harish@gmail.com","1234567"));
-				list.add(new User(2,"harish","harish@gmail.com","1234567"));
-				list.add(new User(3,"harish","harish@gmail.com","1234567"));
-				
-		when(userRepository.findAll()).thenReturn(list);
-		List<User> user = userServiceImpl.getUser();
-		assertThat(user).isEqualTo(list);
-	}
-	
-	@Test
-	public void checkEmailUniqueTetsFail() {
-		String email="kala1@gmail.com";
-		when(userRepository.findByEmail(email)).thenReturn(null);
-		String checkEmailUnique = userServiceImpl.checkEmailUnique(email);
-		assertEquals("No User found with email : "+email, checkEmailUnique);
-	}
-	
-	@Test
-	public void updateUserTest() throws UserNotFoudException {
-		UpdatePwd updatePwd = new UpdatePwd(1,"hari5678","hari123","hari123");
-		User user = new User();
-		BeanUtils.copyProperties(updatePwd, user);
-		when(userRepository.findById(1)).thenReturn(Optional.of(user));
-		
-		String updatePwd2 = userServiceImpl.updatePwd(updatePwd);
-		
-		assertEquals("User Entered the Wrong Password", updatePwd2);
-	}
-	
-	
-	@Test
-	public void forgotPwdTest() throws UserNotFoudException {
-		String email="kala1@gmail.com";
-		when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
-		String forgotPwd = userServiceImpl.forgotPwd(email);
-		assertEquals("User Not Found With This Email : "+email, forgotPwd);
-	}
+//	
+//	@Test
+//	public void signInTest() throws UserNotFoudException {
+//			SignInDto signInDto = new SignInDto();
+//			signInDto.setEmail("shilpa@gmail.com");
+//			signInDto.setPassword("Shil6610");
+//			User user = new User();
+//			when(userRepository.findByEmailAndConfirmPwd(signInDto.getEmail(), signInDto.getPassword())).thenReturn(Optional.of(user));
+//			boolean signin = userServiceImpl.signin(signInDto);
+//			assertEquals(true, signin);
+//	}
+//	
+////	@Test
+////	public void sigiTest() {
+////		SignInDto signInDto = new SignInDto();
+////		assertThrows(UserNotFoudException.class, ()->userServiceImpl.signin(signInDto));
+////	}
+//
+//	
+////	@Test
+////	public void forgotPasswordTest() throws UserNotFoudException {
+////		User user = new User();
+////		when(userRepository.findByEmail("kala1@gmail.com")).thenReturn(Optional.of(user));
+////		String forgotPwd = userServiceImpl.forgotPwd("kala1@gmail.com");
+////		System.out.println(forgotPwd);
+////		assertThat("Harish").isEqualTo(forgotPwd);
+////	}
+//	
+//	@Test
+//	public void getUsersTest() {
+//		List<User> list=new ArrayList<>();
+//				list.add(new User(1,"harish","harish@gmail.com","1234567"));
+//				list.add(new User(2,"harish","harish@gmail.com","1234567"));
+//				list.add(new User(3,"harish","harish@gmail.com","1234567"));
+//				
+//		when(userRepository.findAll()).thenReturn(list);
+//		List<User> user = userServiceImpl.getUser();
+//		assertThat(user).isEqualTo(list);
+//	}
+//	
+//	@Test
+//	public void checkEmailUniqueTetsFail() {
+//		String email="kala1@gmail.com";
+//		when(userRepository.findByEmail(email)).thenReturn(null);
+//		String checkEmailUnique = userServiceImpl.checkEmailUnique(email);
+//		assertEquals("No User found with email : "+email, checkEmailUnique);
+//	}
+//	
 //	@Test
 //	public void updateUserTest() throws UserNotFoudException {
-//		UpdatePwd pwd = new UpdatePwd("Shil6610","Shilpa","Shilpa");
+//		UpdatePwd updatePwd = new UpdatePwd(1,"hari5678","hari123","hari123");
 //		User user = new User();
-//		BeanUtils.copyProperties(pwd, user);
+//		BeanUtils.copyProperties(updatePwd, user);
+//		when(userRepository.findById(1)).thenReturn(Optional.of(user));
 //		
-//		when(userRepository.save(user)).thenReturn(user);
+//		String updatePwd2 = userServiceImpl.updatePwd(updatePwd);
 //		
-//		String updatePwd = userServiceImpl.updatePwd(pwd);
-//		System.out.println(updatePwd);
-//		assertEquals("Password Saved Succesfully", updatePwd);
+//		assertEquals("User Entered the Wrong Password", updatePwd2);
 //	}
-	
-	
-	
-	
-
-}
+//	
+//	
+//	@Test
+//	public void forgotPwdTest() throws UserNotFoudException {
+//		String email="kala1@gmail.com";
+//		when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+//		String forgotPwd = userServiceImpl.forgotPwd(email);
+//		assertEquals("User Not Found With This Email : "+email, forgotPwd);
+//	}
+////	@Test
+////	public void updateUserTest() throws UserNotFoudException {
+////		UpdatePwd pwd = new UpdatePwd("Shil6610","Shilpa","Shilpa");
+////		User user = new User();
+////		BeanUtils.copyProperties(pwd, user);
+////		
+////		when(userRepository.save(user)).thenReturn(user);
+////		
+////		String updatePwd = userServiceImpl.updatePwd(pwd);
+////		System.out.println(updatePwd);
+////		assertEquals("Password Saved Succesfully", updatePwd);
+////	}
+//	
+//	
+//	
+//	
+//
+//}
